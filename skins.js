b3ac61e94c6807ee666b8b1840d4a3c6e6dcab0f
William Bruntrager <w.bruntrager@gmail.com>
2019-09-16 05:21:39 -0400

    Use Original names

---
+++ skins.js
@@ -0,0 +1,24 @@
+var skins = {
+    hitler: {
+        big_bad: 'Hitler',
+        good_guy_id: 'Liberal',
+        bad_guy_id: 'Fascist',
+        good_guy_group: 'Liberals',
+        bad_guy_group: 'Fascists',
+        good_guy_policy: 'Liberal',
+        bad_guy_policy: 'Fascist',
+        president_title: 'President',
+        chancellor_title: 'Chancellor'    
+    },
+    potter: {
+        big_bad: 'Voldemort',
+        good_guy_id: 'Fénix',
+        bad_guy_id: 'Mortífago',
+        good_guy_group: 'Fénix',
+        bad_guy_group: 'Mortífagos',
+        good_guy_policy: 'Protección de Muggles',
+        bad_guy_policy: 'Pureza de Sangre',
+        president_title: 'Presidente',
+        chancellor_title: 'Canciller'    
+    }
+  }
